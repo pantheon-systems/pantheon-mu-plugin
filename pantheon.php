947f323b996f2c403309ca85ce0e9f9f954612e6
Kevin Porras <kevin.porras@pantheon.io>
2022-12-05 11:19:55 -0600

    [CMS-1129] Make network setup show Pantheon appropiate instructions. (#4)
    * Make network setup show Pantheon appropiate instructions.
    
    * Address some PR feedback.
    
    * Allow filename and contents altering.
    
    * Remove if file_exists .htaccess.
    
    * Apply suggestions from code review
    
    Co-authored-by: Chris Reynolds <chris@jazzsequence.com>
    
    Co-authored-by: Chris Reynolds <chris@jazzsequence.com>

--- pantheon.php
+++ pantheon.php
@@ -25,6 +25,9 @@ if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
     if ( defined( 'WP_CLI' ) && WP_CLI ) {
         require_once 'inc/cli.php';
     }
+	if ( defined( 'MULTISITE' ) && defined( 'WP_ALLOW_MULTISITE' ) && WP_ALLOW_MULTISITE ) {
+		require_once 'inc/pantheon-network-setup.php';
+	}
 	if ( ! defined( 'FS_METHOD' ) ) {
 		/**
 		 * When this constant is not set, WordPress writes and then deletes a
