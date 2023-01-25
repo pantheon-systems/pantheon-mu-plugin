040b71fabe24dcc3d6c7303877ec1603919c7d7e
Ryan Wagner <ryan.wagner@pantheon.io>
2023-01-25 17:07:01 -0500

    Adds notification to finish installing the Site Network where appropriate.

---
+++ inc/pantheon-multisite-finalize.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * If a site has multisite enabled, but has not had the final installation
+ * steps completed, alert the user and provide links.
+ */
+
+function pantheon_multisite_install_finalize_message() {
+	?>
+	<div class="notice notice-info is-dismissible">
+		<p>Your WordPress Site Network is almost ready!</p>
+		<p>Visit <a href="https://pantheon.io/docs/guides/multisite/config/#install-the-wordpress-site-network">Pantheon WordPress Site Network Configuration</a>
+		for documentation on how to finalize configuration of your site network.</p>
+	</div>
+	<?php
+}
+
+add_action('admin_notices', 'pantheon_multisite_install_finalize_message');
