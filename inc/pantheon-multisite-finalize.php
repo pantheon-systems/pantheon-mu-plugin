fce9b656148f93a108e394d2184777880762a288
J Ryan Wagner <jryanwagner00@gmail.com>
2023-01-26 15:18:23 -0500

    Removes extraneous line
    Co-authored-by: John Spellman <john.spellman@pantheon.io>

--- inc/pantheon-multisite-finalize.php
+++ inc/pantheon-multisite-finalize.php
@@ -9,7 +9,7 @@ function pantheon_multisite_install_finalize_message() {
 	<div class="notice notice-info is-dismissible">
 		<p><?php esc_html_e( 'Your WordPress Site Network is almost ready!', 'pantheon' ); ?></p>
 		<p><?php echo sprintf( __( 'Visit <a href="%s">Pantheon WordPress Site Network Configuration</a> for documentation on how to finalize configuration of your site network.', 'pantheon-systems' ), 'https://pantheon.io/docs/guides/multisite/config/#install-the-wordpress-site-network' ); ?></p>
-		for documentation on how to finalize configuration of your site network.</p>
+		~~for documentation on how to finalize configuration of your site network.</p> ~~
 	</div>
 	<?php
 }
