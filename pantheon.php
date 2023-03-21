a185eeb0b424fe3e347bc064eae4c7af90ad3a6e
J Ryan Wagner <jryanwagner00@gmail.com>
2023-03-21 16:57:23 -0400

    CMSP-9: Defines multisite variable where appropriate. (#11)
    * CMSP-9: Defines multisite variable where appropriate.
    
    * Fix spacing issue
    
    Co-authored-by: Chris Reynolds <chris@jazzsequence.com>
    
    ---------
    
    Co-authored-by: Ryan Wagner <ryan.wagner@pantheon.io>
    Co-authored-by: Chris Reynolds <chris@jazzsequence.com>

--- pantheon.php
+++ pantheon.php
@@ -40,6 +40,11 @@ if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ) {
 		 */
 		define( 'FS_METHOD', 'direct' );
 	}
+    // When developing a WordPress Multisite locally, ensure that this constant is set.
+    // This will set the Multisite variable in all Pantheon environments.
+    if ( getenv( 'FRAMEWORK' ) === 'wordpress_network' && ! defined( 'WP_ALLOW_MULTISITE' ) ) {
+        define( 'WP_ALLOW_MULTISITE', true );
+    }
     if ( defined( 'WP_ALLOW_MULTISITE' ) && ( ! defined( 'MULTISITE' ) || empty( MULTISITE ) ) ) {
         require_once 'inc/pantheon-multisite-finalize.php';
     }
