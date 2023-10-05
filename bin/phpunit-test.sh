3f592245c05600a50943361271f7d65ffc7a26bb
Chris Reynolds <chris@jazzsequence.com>
2023-10-05 11:40:23 -0600

    colors

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -8,7 +8,7 @@ echo "🤔 Installing WP Unit tests..."
 bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest
 
 echo "🏃‍♂️ Running PHPUnit on Single Site"
-composer phpunit
+composer phpunit --ansi
 
 echo "🧹 Removing files before testing nightly WP..."
 rm -rf $WP_TESTS_DIR $WP_CORE_DIR
@@ -16,8 +16,8 @@ rm -rf $WP_TESTS_DIR $WP_CORE_DIR
 echo "🤔 Installing WP Unit tests with WP nightly version..."
 bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 nightly true
 echo "🏃‍♂️ Running PHPUnit on Single Site (Nightly WordPress)"
-composer phpunit
+composer phpunit --ansi
 
 bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest true
 echo "🏃‍♂️ Running PHPUnit on Multisite"
-composer test:multisite
+composer test:multisite --ansi
