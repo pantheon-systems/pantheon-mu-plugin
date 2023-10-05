bb75dda3119648b809e67d3ca6db2a08596b528c
Chris Reynolds <chris@jazzsequence.com>
2023-10-05 10:25:52 -0600

    change emoji to a broom so it doesn't look like a failure

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -10,7 +10,7 @@ bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest
 echo "🏃‍♂️ Running PHPUnit on Single Site"
 composer phpunit
 
-echo "❌ Removing files before testing nightly WP..."
+echo "🧹 Removing files before testing nightly WP..."
 rm -rf $WP_TESTS_DIR $WP_CORE_DIR
 
 echo "🤔 Installing WP Unit tests with WP nightly version..."
