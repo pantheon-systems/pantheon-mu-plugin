65ef664216a5677687a2014726d49e29c9ecffeb
Chris Reynolds <chris@jazzsequence.com>
2023-10-05 08:23:31 -0600

    add some initial phpunit scaffolding

---
+++ tests/phpunit/bootstrap.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Bootstrap PHPUnit
+ *
+ * @package Pantheon HUD
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+define( 'WP_TESTS_PHPUNIT_POLYFILLS_PATH', __DIR__ . '/../../vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php' );
+
+require_once $_tests_dir . '/includes/functions.php';
+/**
+ * Manually Load Plugin.
+ */
+function _manually_load_plugin() {
+	require dirname( dirname( dirname( __FILE__ ) ) ) . '/pantheon.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+require $_tests_dir . '/includes/bootstrap.php';
