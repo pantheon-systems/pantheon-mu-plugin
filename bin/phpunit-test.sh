1b63a6af9ef9fd415aaf37bc74fa7a770b168372
Chris Reynolds <chris@jazzsequence.com>
2023-12-08 15:12:58 -0700

    copy the wp-latest.json file out of the tmp directory into the tests this way we can use that file to validate against the version of WP that WP is reporting for itself and remove the hard-coded versions

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -5,16 +5,22 @@ set -e
 DIRNAME=$(dirname "$0")
 
 echo "🤔 Installing WP Unit tests..."
-bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest
+bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest true
+
+echo "📄 Copying wp-latest.json..."
+cp /tmp/wp-latest.json "${DIRNAME}/../tests/wp-latest.json"
 
 echo "🏃‍♂️ Running PHPUnit on Single Site"
 composer phpunit --ansi
 
 echo "🧹 Removing files before testing nightly WP..."
 rm -rf "$WP_TESTS_DIR" "$WP_CORE_DIR"
+rm "${DIRNAME}/../tests/wp-latest.json"
 
 echo "🤔 Installing WP Unit tests with WP nightly version..."
 bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 nightly true
+echo "📄 Copying wp-latest.json..."
+cp /tmp/wp-latest.json "${DIRNAME}/../tests/wp-latest.json"
 echo "🏃‍♂️ Running PHPUnit on Single Site (Nightly WordPress)"
 composer phpunit --ansi
 
