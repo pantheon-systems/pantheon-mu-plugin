a4275c6eb55372d0617fac90d2d0ffe9801cad88
Chris Reynolds <chris@jazzsequence.com>
2023-12-08 16:03:15 -0700

    remove the true (for no db install) that i used for testing

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -5,7 +5,7 @@ set -e
 DIRNAME=$(dirname "$0")
 
 echo "🤔 Installing WP Unit tests..."
-bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest true
+bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest
 
 echo "📄 Copying wp-latest.json..."
 cp /tmp/wp-latest.json "${DIRNAME}/../tests/wp-latest.json"
