133425b47f96581c03244dce9675d558f309a5c4
Chris Reynolds <chris.reynolds@pantheon.io>
2023-12-12 09:21:21 -0700

    Remove hard-coded WP latest version check (#28)
    * copy the wp-latest.json file out of the tmp directory into the tests
    this way we can use that file to validate against the version of WP that WP is reporting for itself and remove the hard-coded versions
    
    * adds a helper method that gets the wp version from wp-latest.json
    
    * use the version stored in wp-latest.json
    
    * remove commented out var_dump
    
    * ignore the vip sniff
    
    * remove the true (for no db install) that i used for testing

--- tests/phpunit/test-pantheon-updates.php
+++ tests/phpunit/test-pantheon-updates.php
@@ -25,6 +25,24 @@ class Test_Pantheon_Updates extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Get the latest WordPress version from the wp-latest.json file.
+	 * 
+	 * @return string|bool The latest WordPress version or false if the file doesn't exist.
+	 */
+	private static function get_latest_wp_version_from_file() {
+		$file = dirname( __DIR__ ) . '/wp-latest.json';
+
+		if ( ! file_exists( $file ) ) {
+			return false;
+		}
+
+		$version_raw = json_decode( file_get_contents( $file ) );
+		$version = $version_raw->offers[0]->current;
+
+		return $version;
+	}
+
+	/**
 	 * Test the _pantheon_hide_update_nag function.
 	 */
 	public function test_pantheon_hide_update_nag() {
@@ -46,9 +64,10 @@ class Test_Pantheon_Updates extends WP_UnitTestCase {
 	public function test_pantheon_get_current_wordpress_version() {
 		// Run the function.
 		$result = _pantheon_get_current_wordpress_version();
+		$current_version = self::get_latest_wp_version_from_file();
 
 		// Check that the returned version is correct. This value needs to be changed when the WordPress version is updated.
-		$this->assertEquals( '6.4.1', $result );
+		$this->assertEquals( $current_version, $result );
 	}
 
 	/**
