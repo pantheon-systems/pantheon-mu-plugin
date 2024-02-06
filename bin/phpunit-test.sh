439372c0926b2fd334972dcc2a848a3ca5b71602
Chris Reynolds <chris@jazzsequence.com>
2024-02-06 11:33:44 -0700

    move multisite tests to run before nightly tests

--- bin/phpunit-test.sh
+++ bin/phpunit-test.sh
@@ -13,6 +13,14 @@ cp /tmp/wp-latest.json "${DIRNAME}/../tests/wp-latest.json"
 echo "🏃‍♂️ Running PHPUnit on Single Site"
 composer phpunit --ansi
 
+echo "🧹 Removing files before testing WPMS..."
+rm -rf "$WP_TESTS_DIR" "$WP_CORE_DIR"
+rm "${DIRNAME}/../tests/wp-latest.json"
+
+bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest true
+echo "🏃‍♂️ Running PHPUnit on Multisite"
+composer test:multisite --ansi
+
 echo "🧹 Removing files before testing nightly WP..."
 rm -rf "$WP_TESTS_DIR" "$WP_CORE_DIR"
 rm "${DIRNAME}/../tests/wp-latest.json"
@@ -23,7 +31,3 @@ echo "📄 Copying wp-latest.json..."
 cp /tmp/wp-latest.json "${DIRNAME}/../tests/wp-latest.json"
 echo "🏃‍♂️ Running PHPUnit on Single Site (Nightly WordPress)"
 composer phpunit --ansi
-
-bash "${DIRNAME}/install-wp-tests.sh" wordpress_test root root 127.0.0.1 latest true
-echo "🏃‍♂️ Running PHPUnit on Multisite"
-composer test:multisite --ansi
