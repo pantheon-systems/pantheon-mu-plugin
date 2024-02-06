6e51dd8227d1672e4b8f47476231edcfde2675c9
Chris Reynolds <chris@jazzsequence.com>
2024-02-06 09:04:10 -0700

    test the pantheon_font_dir function works as expected

--- tests/phpunit/test-fonts.php
+++ tests/phpunit/test-fonts.php
@@ -5,6 +5,8 @@
  * @package pantheon
  */
 
+Use Pantheon\Fonts;
+
 /**
  * Main Mu Plugin Test Case
  */
@@ -18,5 +20,30 @@ class Test_Fonts extends WP_UnitTestCase
 		$this->assertEquals( has_action( 'init', 'Pantheon\Fonts\bootstrap' ), 10 );
 		$this->assertTrue( defined( 'PANTHEON_MODIFY_FONTS_DIR' ) );
 		$this->assertTrue( PANTHEON_MODIFY_FONTS_DIR );
+	}
+
+	/**
+	 * Test the pantheon_font_dir function.
+	 */	
+	public function test_pantheon_font_dir() {
+		$this->assertTrue( function_exists( 'Pantheon\Fonts\pantheon_font_dir' ) );
+		
+		if ( ! function_exists( 'wp_get_font_dir' ) ) {
+			// Skip the test if wp_get_font_dir does not exist.
+			$this->markTestSkipped( 'wp_get_font_dir does not exist. WP 6.5+ or Gutenberg 17.6+ must be available to test the font library modifications.' );
+		}
+		$default_fonts = wp_get_font_dir();
+		$font_dir = Fonts\pantheon_font_dir( $default_fonts );
+
+		$this->assertNotEquals( $default_fonts, $font_dir );
+		$this->assertEquals( array_keys( $default_fonts ), array_keys( $font_dir ) );
+		$this->assertArrayHasKey( 'path', $font_dir );
+		$this->assertArrayHasKey( 'url', $font_dir );
+		$this->assertArrayHasKey( 'basedir', $font_dir );
+		$this->assertArrayHasKey( 'baseurl', $font_dir );
+		$this->assertStringContainsString( 'uploads/fonts', $font_dir['path'] );
+		$this->assertStringContainsString( 'uploads/fonts', $font_dir['url'] );
+		$this->assertStringContainsString( 'uploads/fonts', $font_dir['basedir'] );
+		$this->assertStringContainsString( 'uploads/fonts', $font_dir['baseurl'] );
 	}
 }
