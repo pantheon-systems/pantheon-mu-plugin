cd021bded76299846431094539a332d68cbf7119
Chris Reynolds <chris@jazzsequence.com>
2024-02-06 10:20:22 -0700

    Add test for our font directory filtering

--- tests/phpunit/test-fonts.php
+++ tests/phpunit/test-fonts.php
@@ -55,6 +55,34 @@ class Test_Fonts extends WP_UnitTestCase
 	}
 
 	/**
+	 * Test that our filtered font directory is filtered properly.
+	 */
+	public function test_pantheon_font_dir_filter() {
+		// Check current WP version to see if we can run the test.
+		$version = _pantheon_get_current_wordpress_version();
+		if ( version_compare( $version, '6.4', '<=' ) ) {
+			// Skip the test if the current WP version is less than 6.5.
+			$this->markTestSkipped( 'WP 6.5+ or Gutenberg 17.6+ must be available to test the font library modifications.' );
+		}
+		
+		$this->maybe_get_font_library();
+
+		add_filter( 'font_dir', '\\Pantheon\\Fonts\\pantheon_font_dir' );
+		$font_dir = wp_get_font_dir();
+		
+		$expected = [
+			'path' => WP_CONTENT_DIR . '/uploads/fonts',
+			'url' => WP_CONTENT_URL . '/uploads/fonts',
+			'subdir' => '',
+			'basedir' => WP_CONTENT_DIR . '/uploads/fonts',
+			'baseurl' => WP_CONTENT_URL . '/uploads/fonts',
+			'error' => false,
+		];
+
+		$this->assertEquals( $expected, $font_dir );
+	}
+
+	/**
 	 * Get the font library from Gutenberg if it's not available.
 	 */
 	private function maybe_get_font_library() {
