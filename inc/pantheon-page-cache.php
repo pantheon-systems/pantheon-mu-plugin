e8b3c517960067283328fdbc8b191c0da4fe8f10
Chris Reynolds <chris.reynolds@pantheon.io>
2024-05-15 15:42:08 -0600

    [BUGS-8085] Use filtered value when sending cache control headers (#45)
    * use the filter when sending cache control headers
    
    * lint
    
    * add a test for the max-age value(s)
    
    * add better messaging for failed tests

--- inc/pantheon-page-cache.php
+++ inc/pantheon-page-cache.php
@@ -363,7 +363,8 @@ class Pantheon_Cache {
 	 */
 	private function get_cache_control_header_value() {
 		if ( ! is_admin() && ! is_user_logged_in() ) {
-			$ttl = absint( $this->options['default_ttl'] );
+			// Use the filtered default max-age if applicable.
+			$ttl = apply_filters( 'pantheon_cache_default_max_age', absint( $this->options['default_ttl'] ) );
 			if ( $ttl < 60 && isset( $_ENV['PANTHEON_ENVIRONMENT'] ) && 'live' === $_ENV['PANTHEON_ENVIRONMENT'] ) {
 				$ttl = 60;
 			}
