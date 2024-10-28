92dd4dc35103ef01e55ce1abe62abcc0cc99fa35
Phil Tyler <phil.tyler@pantheon.io>
2024-10-28 14:23:53 -0700

    comments

--- inc/pantheon-media.php
+++ inc/pantheon-media.php
@@ -1,8 +1,11 @@
 <?php 
+/**
+ * Media handling adjustments while on Pantheon.
+ **/
 
-/*
+/**
  * HEIC support added in 6.7 but not yet supported by ImageMagick on Pantheon.
- */
+ **/
 add_filter( 'image_editor_output_format', function ( $output_format ) {
 	if ( isset( $output_format['image/heic'] ) ) {
 		unset( $output_format['image/heic'] );
