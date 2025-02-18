c5b2e1060b33dc2d23b84b70f4022acf81eeef2f
Chris Reynolds <chris@jazzsequence.com>
2025-02-18 10:43:01 -0700

    fix plugin_compatibility array key check pull out the isset from the conditional so we can save it to a variable and define a default (false) state, then fix the broken conditional

--- inc/site-health.php
+++ inc/site-health.php
@@ -151,9 +151,10 @@ function output_compatibility_status_table( $plugins, $output = true, $incompati
 		<?php
 
 		// Filter out incompatible plugins. This allows us to re-use the status table for different types of compatibility issues.
-		if ( $incompatible && ( ! isset( $plugins['plugin_compatibility'] ) || $plugins['plugin_compatibility'] !== 'incompatible' ) ) {
+		$plugin_compatibility = isset( $plugins['plugin_compatibility'] ) ? $plugins['plugin_compatibility'] : false;
+		if ( $incompatible && ( ! $plugin_compatibility || $plugin_compatibility !== 'incompatible' ) ) {
 			$plugins = array_filter( $plugins, function ( $plugins ) {
-				return $plugins['plugin_compatibility'] === 'incompatible';
+				return $plugin_compatibility === 'incompatible';
 			} );
 		}
 
